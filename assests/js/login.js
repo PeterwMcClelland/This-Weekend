540d613293e1568ee61d1b50c77691f1693d28ca
Peter McClelland <peterwmcclelland@gmail.com>
2022-10-09 15:11:15 -0500

    start of login page

--- assests/js/login.js
+++
@@ -1,9 +0,0 @@
-async function basiclogin (email, password) {
-    const response = await zlFetch.post(loginEndpoint, {
-      auth: {
-        username: email,
-        password: password
-      },
-      body: { /*...*/ }
-    })
-  }
